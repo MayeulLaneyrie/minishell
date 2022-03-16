0787cf1272cdd0e783ec898c7d9e9f502d1570a2
Mayeul LANEYRIE <mlaneyri@student.42.fr>
2022-03-16 16:13:02 +0100

    Fixed norm in ft_lstclear.c + Defined t_cmd & t_sh in minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mlaneyri <mlaneyri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:39:41 by mlaneyri          #+#    #+#             */
-/*   Updated: 2022/03/16 01:38:28 by mlaneyri         ###   ########.fr       */
+/*   Updated: 2022/03/16 16:09:36 by mlaneyri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,41 @@
 # include <stdio.h>
 # include <unistd.h>
 
+# include <string.h>
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+# include <dirent.h>
+
+# include <signal.h>
+
 # include "../libft/libft.h"
+
+/*
+**	constdest.h holds the prototypes of the constructor and destructor functions
+*/
+# include "./consdest.h"
+
+typedef struct s_cmd
+{
+	int		ac;
+	char	**av;
+}	t_cmd;
+
+/*
+**	xt_status : The exit status of the last executed job. xt_stat should be the
+**	return value of main().
+*/
+typedef struct s_sh
+{
+	t_cmd	*cmd;
+	int		xt_stat;
+}	t_sh;
 
 #endif
--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,20 +6,18 @@
 /*   By: mlaneyri <mlaneyri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 23:10:23 by mlaneyri          #+#    #+#             */
-/*   Updated: 2022/03/15 16:45:40 by mlaneyri         ###   ########.fr       */
+/*   Updated: 2022/03/16 15:48:14 by mlaneyri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
+static void	ft_lstclearall(t_list *lst, void (*del)(void *))
 {
 	t_list	*cpy;
 	t_list	*cpy1;
 
-	if (!*lst)
-		return ;
-	cpy = *lst;
+	cpy = lst;
 	cpy1 = cpy->next;
 	while (cpy1)
 	{
@@ -28,7 +26,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		cpy1 = cpy1->next;
 		free(cpy);
 	}
-	cpy = *lst;
+	cpy = lst;
 	cpy1 = cpy->prev;
 	while (cpy1)
 	{
@@ -37,6 +35,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		cpy1 = cpy1->prev;
 		free(cpy);
 	}
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (!*lst)
+		return ;
+	ft_lstclearall(*lst, del);
 	ft_lstdelone(*lst);
 	free(*lst);
 	*lst = NULL;
