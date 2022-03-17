1c7cbc719eb6a7799d722d1858745f5b14d10971
Benjamin <benj.ifrah@gmail.com>
2022-03-17 21:52:38 +0100

    utilisation envp

--- src/main.c
+++ src/main.c
@@ -6,16 +6,18 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:38:16 by mlaneyri          #+#    #+#             */
-/*   Updated: 2022/03/17 21:49:26 by bifrah           ###   ########.fr       */
+/*   Updated: 2022/03/17 21:50:33 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	t_sh	*sh;
 
+	(void)argc;
+	(void)argv;
 	write(0, "Welcome to minishell \"MarkI\"!\n", 30);
 	sh = new_sh();
 	if (!sh)
