4fdec03de2a3d541a67b8a5f6324864639592b0a
Mayeul Laneyrie <mayeul.lnr@gmail.com>
2022-03-17 23:02:37 +0100

    changed name of argc->ac and argv->av

--- src/main.c
+++ src/main.c
@@ -6,18 +6,18 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:38:16 by mlaneyri          #+#    #+#             */
-/*   Updated: 2022/03/17 21:50:33 by bifrah           ###   ########.fr       */
+/*   Updated: 2022/03/17 22:07:51 by mlaneyri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+int	main(int ac, char **av, char **envp)
 {
 	t_sh	*sh;
 
-	(void)argc;
-	(void)argv;
+	(void)ac;
+	(void)av;
 	write(0, "Welcome to minishell \"MarkI\"!\n", 30);
 	sh = new_sh();
 	if (!sh)
