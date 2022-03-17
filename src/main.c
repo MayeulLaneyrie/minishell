bd6d5a9ceb76144370f48282176729f369489db9
Mayeul Laneyrie <mayeul.lnr@gmail.com>
2022-03-17 20:17:33 +0100

    First version of main TODO: implement main_part1 and main_part2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlaneyri <mlaneyri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:38:16 by mlaneyri          #+#    #+#             */
-/*   Updated: 2022/03/17 20:00:17 by mlaneyri         ###   ########.fr       */
+/*   Updated: 2022/03/17 20:16:41 by mlaneyri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int	main(void)
 {
+	t_sh	*sh;
+
 	write(0, "Welcome to minishell \"MarkI\"!\n", 30);
+	sh = new_sh();
+	if (!sh)
+		return (-1);
+	while (!main_part1(sh))
+	{
+		if (main_part2(sh))
+			break;
+	}
+	del_sh(&sh);
 	return (0);
 }
