0ccddae4fc068eb9662b0b05252ddd328a5ca76f
Benjamin <benj.ifrah@gmail.com>
2022-04-09 14:14:10 +0200

    reorga + modif fill_cmd and all fct() in fill_cmd

--- src/main_part1.c
+++ src/main_part1.c
@@ -1,5 +1,74 @@
 #include "../include/minishell.h"
 
+void	clean_d_quote(t_sh *sh)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	j = 0;
+	while (sh->cmd->av[j])
+	{
+		i = 0;
+		if (sh->cmd->av[j][i] == '"')
+		{
+			tmp = (char *)malloc(sizeof(char) * ft_strlen(sh->cmd->av[j]) - 2);
+			while (sh->cmd->av[j][i + 1])
+			{
+				tmp[i] = sh->cmd->av[j][i + 1];
+				i++;
+			}
+			free(sh->cmd->av[j]);
+			sh->cmd->av[j] = ft_strdup(tmp);
+			free(tmp);
+		}
+		j++;
+	}
+}
+
+void	word_cpy(t_sh *sh, t_rl *rl, int i, int j)
+{
+	int	k;
+
+	k = 0;
+	rl->d_quote = false;
+	rl->quote = false;
+	if (rl->rdline[i] != '"' && rl->rdline[i] != '\'')
+	{
+		while (rl->rdline[i] && !(is_meta(rl->rdline[i]))
+			&& (rl->d_quote == false || rl->quote == false))
+		{
+			sh->cmd->av[j][k++] = rl->rdline[i++];
+			if (rl->rdline[i] == '"' && rl->d_quote == false
+				&& is_meta(rl->rdline[i - 1]))
+				rl->d_quote = true;
+			if (rl->rdline[i] == '\'' && rl->quote == false
+				&& is_meta(rl->rdline[i - 1]))
+				rl->quote = true;
+		}
+	}
+	else if (rl->d_quote == true && rl->d_quote == false && rl->quote == false)
+	{
+		rl->d_quote = true;
+		while (rl->rdline[i] != '"' && rl->d_quote == true)
+		{
+			sh->cmd->av[j][k++] = rl->rdline[i++];
+			if (rl->rdline[i] == '"')
+				rl->d_quote = false;
+		}
+	}
+	else if (rl->quote == true && rl->d_quote == false && rl->quote == false)
+	{
+		rl->quote = true;
+		while (rl->rdline[i] != '\'' && rl->quote == true)
+		{
+			sh->cmd->av[j][k++] = rl->rdline[i++];
+			if (rl->rdline[i] == '\'')
+				rl->quote = false;
+		}
+	}
+}
+
 int	word_len(t_rl *rl, int i)
 {
 	int		tmp;
@@ -44,90 +113,8 @@ int	word_len(t_rl *rl, int i)
 	return (i - tmp);
 }
 
-void	word_cpy(t_sh *sh, t_rl *rl, int i, int j)
-{
-	int	k;
-
-	k = 0;
-	rl->d_quote = false;
-	rl->quote = false;
-	printf("i1 = %d\n", i);
-	if (rl->rdline[i] != '"' && rl->rdline[i] != '\'')
-	{
-		printf("i2 = %d\n", i);
-		while (rl->rdline[i] && !(is_meta(rl->rdline[i]))
-			&& (rl->d_quote == false || rl->quote == false))
-		{
-			printf("i3.1.1 = %d\n", i);
-			printf("j3.1.1 = %d\n", j);
-			printf("k3.1.1 = %d\n", k);
-			sh->cmd->av[j][k++] = rl->rdline[i++];
-			if (rl->rdline[i] == '"' && rl->d_quote == false
-				&& is_meta(rl->rdline[i - 1]))
-				rl->d_quote = true;
-			if (rl->rdline[i] == '\'' && rl->quote == false
-				&& is_meta(rl->rdline[i - 1]))
-				rl->quote = true;
-			printf("i3.1.2 = %d\n", i);
-			printf("j3.1.2 = %d\n", j);
-			printf("k3.1.2 = %d\n", k);
-		}
-	}
-	else if (rl->d_quote == true && rl->d_quote == false && rl->quote == false)
-	{
-		rl->d_quote = true;
-		while (rl->rdline[i] != '"' && rl->d_quote == true)
-		{
-			printf("i3.2 = %d\n", i);
-			sh->cmd->av[j][k++] = rl->rdline[i++];
-			if (rl->rdline[i] == '"')
-				rl->d_quote = false;
-		}
-	}
-	else if (rl->quote == true && rl->d_quote == false && rl->quote == false)
-	{
-		rl->quote = true;
-		while (rl->rdline[i] != '\'' && rl->quote == true)
-		{
-			printf("i3.3 = %d\n", i);
-			sh->cmd->av[j][k++] = rl->rdline[i++];
-			if (rl->rdline[i] == '\'')
-				rl->quote = false;
-		}
-	}
-}
-
-void	clean_d_quote(t_sh *sh)
-{
-	int		i;
-	int		j;
-	char	*tmp;
-
-	j = 0;
-	// printf("old_av[1] = %s\n", sh->cmd->av[1]);
-	while (sh->cmd->av[j])
-	{
-		i = 0;
-		if (sh->cmd->av[j][i] == '"')
-		{
-			tmp = (char *)malloc(sizeof(char) * ft_strlen(sh->cmd->av[j]) - 2);
-			while (sh->cmd->av[j][i + 1])
-			{
-				tmp[i] = sh->cmd->av[j][i + 1];
-				i++;
-			}
-			free(sh->cmd->av[j]);
-			sh->cmd->av[j] = ft_strdup(tmp);
-			free(tmp);
-		}
-		j++;
-	}
-	//	printf("new_av[1] = %s\n", sh->cmd->av[1]);
-}
-
 int	fill_cmd(t_sh *sh, t_rl *rl)
 {
-	char	*word;
 	int		i;
 	int		j;
 
@@ -137,13 +124,9 @@ int	fill_cmd(t_sh *sh, t_rl *rl)
 	{
 		while (is_meta(rl->rdline[i]) == true)
 			i++;
-		word = (char *)malloc(sizeof(char) * (word_len(rl, i) + 1));
-		if (!word)
-			return (1);
 		sh->cmd->av = realloc_split(sh);
 		write(1, "i'm in fill_cmd\n", 16);
 		word_cpy(sh, rl, i, j);
-		printf("len : %d\n", word_len(rl, i));
 		i += word_len(rl, i);
 		j++;
 		clean_d_quote(sh);
