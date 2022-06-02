adfc89ca43ab90cdd6f63c7d1ecc31d18a328afe
bifrah <benj.ifrah@gmail.com>
2022-06-02 18:23:08 +0200

    del useless fct

--- src/redirect.c
+++ src/redirect.c
@@ -11,33 +11,3 @@ void	in_out_quotes(char c, int *quote, int *d_quote)
 	else if (c == '\'' && *quote)
 		*quote = 0;
 }
-
-// int	fill_struct_redir(t_sh *sh, char *s)
-// {
-// 	// t_list	*red;
-// 	int		i;
-// 	int		quote;
-// 	int		d_quote;
-// 	int		stck_i_err;
-
-// 	i = 0;
-// 	quote = 0;
-// 	d_quote = 0;
-// 	(void)sh;
-// 	while (s[i])
-// 	{
-// 		in_out_quotes(s[i], &quote, &d_quote);
-// 		if (quote == 0 && d_quote == 0 && (s[i] == '>' || s[i] == '<'))
-// 		{
-// 			if (check_after(s, i))
-// 			{
-// 				stck_i_err = i;
-// 				// create_files(); create file before stck_i_err
-// 				return (1);
-// 			}
-// 		}
-// 		i++;
-// 	}
-// 	// stock;
-// 	return (0);
-// }
